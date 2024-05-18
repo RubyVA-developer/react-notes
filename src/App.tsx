4556970918b6edee6194f4a1600b712c346c76c3
otaku-niisan <honka2000profiletest@gmail.com>
2024-05-18 18:22:39 +0800

    init: add vite, prettier, eslint, base config, json-server

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-2xl text-blue-600 font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
